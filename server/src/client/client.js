8469ba0d634b12cd0697b954cff0a1f9273270fb
jermiedomingo <8jermiedomingo@gmail.com>
2019-05-26 20:21:46 +0200

    client.js init

---
+++ server/src/client/client.js
@@ -0,0 +1,6 @@
+// startup point for the client side application
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./components/Home";
+
+ReactDOM.hydrate(<Home />, document.getElementById("root"));
--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>I'm the best home component</div>;
+  return (
+    <div>
+      <div>I'm the best home component</div>
+      <button onClick={() => console.log("press me")}>Press me</button>
+    </div>
+  );
 };
 
 export default Home;
