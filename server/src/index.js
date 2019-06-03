fe4e18e666cf8f1611fe12b2a7436a2542a8e978
undrafted <8jermiedomingo@gmail.com>
2019-06-03 23:50:50 +0200

    add matchroutes to know which component to render based on the req url

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 import "babel-polyfill";
 import express from "express";
+import { matchRoutes } from "react-router-config";
+import Routes from "./client/Routes";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
 
@@ -10,7 +12,8 @@ app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore();
 
-  // some logic to init and load data to store
+  // to figure out which component to render based on the url
+  matchRoutes(Routes, req.path);
 
   res.send(renderer(req, store));
 });
