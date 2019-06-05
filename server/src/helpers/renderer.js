81c27ae9e18bdddf715fe4782bed65f2cae91a36
undrafted <8jermiedomingo@gmail.com>
2019-06-05 23:49:10 +0200

    xss prevention

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -3,6 +3,11 @@ import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { renderRoutes } from "react-router-config";
+// instead of adding serialize
+// another way is to add initial state (data we dont trust) on a data attribute of the
+// react component, so it will go inside
+// the renderToString method of react
+import serialize from "serialize-javascript";
 import Routes from "../client/Routes";
 
 export default (req, store) => {
@@ -20,7 +25,7 @@ export default (req, store) => {
       <body>
         <div id="root">${content}</div>
         <script>
-          window.INITIAL_STATE = ${JSON.stringify(store.getState())}
+          window.INITIAL_STATE = ${serialize(store.getState())}
         </script>
         <script src="bundle.js"></script>
       </body>
