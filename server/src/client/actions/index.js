0a97f6d9ec596fd8a6c6c2bef8e8eb131c738bd4
undrafted <8jermiedomingo@gmail.com>
2019-06-08 21:04:33 +0200

    auth redux boilerplate

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,11 +1,20 @@
-export const FETCH_USERS = "fetch_users";
-
-// 3rd instance 'api' comes from thunk.withExtraArgument added on createStore
+// 3rd argument 'api' comes from thunk.withExtraArgument added on createStore
 // it is our custom axios instance
+
+export const FETCH_USERS = "fetch_users";
 export const fetchUsers = () => async (dispatch, getState, api) => {
   const res = await api.get("/users");
   dispatch({
     type: FETCH_USERS,
+    payload: res
+  });
+};
+
+export const FETCH_CURRENT_USER = "fetch_current_user";
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+  const res = await api.get("/current_user");
+  dispatch({
+    type: FETCH_CURRENT_USER,
     payload: res
   });
 };
