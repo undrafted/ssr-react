361622c5f70874295e843eea237ffc09f4b21f5e
undrafted <8jermiedomingo@gmail.com>
2019-06-26 23:56:22 +0200

    fix dependencies/babel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "babel-polyfill";
+import "@babel/polyfill";
 import express from "express";
 import { matchRoutes } from "react-router-config";
 import proxy from "express-http-proxy";
--- webpack.client.js
+++ webpack.client.js
@@ -4,7 +4,7 @@ const baseConfig = require("./webpack.base");
 
 const config = {
   // tell webpack the root file of our server application
-  entry: "./src/client/client.js",
+  entry: "./src/client/client.ts",
 
   // tell webpack where to put the output file generated
   output: {
